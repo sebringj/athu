2921a04d55b75c3abfa8380bc20e40f52c52bd3e
Jason Sebring <mail@jasonsebring.com>
2015-09-30 21:19:56 -0700

    nunjucks template added

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ var nunjucks = require('nunjucks');
 var app = express();
 var port = process.env.PORT || config.port || 3000;
 
-nunjucks.configure('views', {
+nunjucks.configure('src/views', {
     autoescape: true,
     express: app
 });
 
-//app.use('/', require('./controllers'));
+app.use('/', require('./controllers'));
 app.use(express.static('public'));
 app.use(favicon(__dirname + '/../public/favicon.ico'));
 
