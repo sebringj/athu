f9b0e159c870952f0d37e7c6fed4fb3046bce5f5
Jason Sebring <mail@jasonsebring.com>
2015-10-16 23:11:23 -0700

    tabs to spaces

--- src/index.js
+++ src/index.js
@@ -8,20 +8,37 @@ var port = process.env.PORT || config.port || 3000;
 var passport = require('passport');
 var passportHelper = require('./utils/passport');
 
-app.use(express.session({ secret: config.sessionSecret }));
+app.use(express.session({
+  secret: config.sessionSecret
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static('public'));
 app.use(favicon(__dirname + '/../public/favicon.ico'));
 
 var providers = {
-  google: { Strategy: require('passport-google-oauth').OAuth2Strategy },
-  facebook: { Strategy: require('passport-facebook').Strategy },
-  dropbox: { authenticate: 'dropbox-oauth2', Strategy: require('passport-dropbox-oauth2').Strategy },
-  twitter: { Strategy: require('passport-twitter').Strategy },
-  github: { Strategy: require('passport-github').Strategy },
-  instagram: { Strategy: require('passport-instagram').Strategy },
-  reddit: { Strategy: require('passport-reddit').Strategy }
+  google: {
+    Strategy: require('passport-google-oauth').OAuth2Strategy
+  },
+  facebook: {
+    Strategy: require('passport-facebook').Strategy
+  },
+  dropbox: {
+    authenticate: 'dropbox-oauth2',
+    Strategy: require('passport-dropbox-oauth2').Strategy
+  },
+  twitter: {
+    Strategy: require('passport-twitter').Strategy
+  },
+  github: {
+    Strategy: require('passport-github').Strategy
+  },
+  instagram: {
+    Strategy: require('passport-instagram').Strategy
+  },
+  reddit: {
+    Strategy: require('passport-reddit').Strategy
+  }
 };
 
 for (let provider of Object.keys(providers)) {
@@ -33,7 +50,7 @@ for (let provider of Object.keys(providers)) {
   });
 }
 
-var server = app.listen(port, function () {
+var server = app.listen(port, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('athu.io listening at http(s)://%s:%s', host, port);
--- src/utils/passport.js
+++ src/utils/passport.js
@@ -5,46 +5,49 @@ var config = require('config');
 var passport = require('passport');
 
 module.exports = {
-	setProvider: function(options) {
+  setProvider: function(options) {
 
-		options.authenticate = options.authenticate || options.provider;
+    options.authenticate = options.authenticate || options.provider;
 
-		passport.use(new options.Strategy(config.providers[options.provider], function() {
-			var args = Array.prototype.slice.call(arguments);
-			var profile = args[args.length - 2];
-			var done = args[args.length - 1];
-			return done(null, { id: profile.id, provider: options.provider });
-	  }));
+    passport.use(new options.Strategy(config.providers[options.provider], function() {
+      var args = Array.prototype.slice.call(arguments);
+      var profile = args[args.length - 2];
+      var done = args[args.length - 1];
+      return done(null, {
+        id: profile.id,
+        provider: options.provider
+      });
+    }));
 
-		app.get(
-			'/auth/' + provider,
-			function setRedirect(req, res, next) {
-				var referer = config.referers[req.headers.referer];
-				if (!referer) {
-					res.status(400).send('bad request');
-					return;
-				}
-				req.session.referer = referer;
-				next();
-			},
-			passport.authenticate(options.authenticate, options.options)
-		);
+    app.get(
+      '/auth/' + provider,
+      function setRedirect(req, res, next) {
+        var referer = config.referers[req.headers.referer];
+        if (!referer) {
+          res.status(400).send('bad request');
+          return;
+        }
+        req.session.referer = referer;
+        next();
+      },
+      passport.authenticate(options.authenticate, options.options)
+    );
 
-		app.get('/auth/' + provider + '/callback', function(req, res, next) {
-			if (!req.session.referer) {
-				res.status(400).send('bad request');
-				return;
-			}
-			passport.authenticate(options.authenticate, function(err, profile, info) {
-				var referer = req.session.referer;
-				req.session.destroy();
-				if (err || !profile) {
-					res.redirect(referer.errorRedirect);
-					return;
-				}
-				res.redirect(referer.successRedirect + '?jwt=' + jwt.getToken(profile));
-			})(req, res, next);
-		});
+    app.get('/auth/' + provider + '/callback', function(req, res, next) {
+      if (!req.session.referer) {
+        res.status(400).send('bad request');
+        return;
+      }
+      passport.authenticate(options.authenticate, function(err, profile, info) {
+        var referer = req.session.referer;
+        req.session.destroy();
+        if (err || !profile) {
+          res.redirect(referer.errorRedirect);
+          return;
+        }
+        res.redirect(referer.successRedirect + '?jwt=' + jwt.getToken(profile));
+      })(req, res, next);
+    });
 
-	}
+  }
 };
