22018d3ff5ee085e1ad488dc6b34318db8332e5b
Jason Sebring <mail@jasonsebring.com>
2015-10-16 23:35:48 -0700

    tabs to spaces

--- config/default.js
+++ config/default.js
@@ -13,33 +13,33 @@ module.exports = {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
-			Strategy: require('passport-google-oauth').OAuth2Strategy
+      Strategy: require('passport-google-oauth').OAuth2Strategy
     },
     facebook: {
       clientID: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
       callbackURL: process.env.FACEBOOK_CALLBACK_URL,
       enableProof: false,
-			Strategy: require('passport-facebook').Strategy
+      Strategy: require('passport-facebook').Strategy
     },
     dropbox: {
       clientID: process.env.DROPBOX_CLIENT_ID,
       clientSecret: process.env.DROPBOX_CLIENT_SECRET,
       callbackURL: process.env.DROPBOX_CALLBACK_URL,
-			authenticate: 'dropbox-oauth2',
-	    Strategy: require('passport-dropbox-oauth2').Strategy
+      authenticate: 'dropbox-oauth2',
+      Strategy: require('passport-dropbox-oauth2').Strategy
     },
     twitter: {
       consumerKey: process.env.TWITTER_CONSUMER_KEY,
       consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
       callbackURL: process.env..TWITTER_CONSUMER_CALLBACK_URL,
-			Strategy: require('passport-twitter').Strategy
+      Strategy: require('passport-twitter').Strategy
     },
     github: {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackURL: process.env.GITHUB_CALLBACK_URL,
-			Strategy: require('passport-github').Strategy
+      Strategy: require('passport-github').Strategy
     },
     instagram: {
       clientID: process.env.INSTAGRAM_CLIENT_ID,
@@ -50,15 +50,15 @@ module.exports = {
       clientID: process.env.REDDIT_CLIENT_ID,
       clientSecret: process.env.REDDIT_CLIENT_SECRET,
       callbackURL: process.env.REDDIT_CALLBACK_URL,
-			Strategy: require('passport-instagram').Strategy
+      Strategy: require('passport-instagram').Strategy
     },
-		linkedin: {
-			clientID: process.env.LINKEDIN_CLIENT_ID,
+    linkedin: {
+      clientID: process.env.LINKEDIN_CLIENT_ID,
       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
       callbackURL: process.env.LINKEDIN_CALLBACK_URL
-		  scope: ['r_emailaddress', 'r_basicprofile'],
-			Strategy: require('passport-linkedin-oauth2').Strategy
-		}
+      scope: ['r_emailaddress', 'r_basicprofile'],
+      Strategy: require('passport-linkedin-oauth2').Strategy
+    }
   },
   referers: {
     localhost: {
