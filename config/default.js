4923a24e0fc837ece9d2db8ed2868bc0840c706b
Jason Sebring <mail@jasonsebring.com>
2015-10-21 10:15:34 -0700

    updated readme

--- config/default.js
+++ config/default.js
@@ -9,6 +9,7 @@ module.exports = {
 
   // List passport-compatible providers installed and setup.
   // Note: callbackURL is set and determined by the app.
+  // Link your "sign in with" button to http(s)://[athu host]/auth/[provider name]
   providers: {
     google: {
       clientID: process.env.GOOGLE_CLIENT_ID,
@@ -16,45 +17,6 @@ module.exports = {
       Strategy: require('passport-google-oauth').OAuth2Strategy,
       scope: ['profile']
     },
-    /*
-    facebook: {
-      clientID: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      enableProof: false,
-      Strategy: require('passport-facebook').Strategy
-    },
-    dropbox: {
-      clientID: process.env.DROPBOX_CLIENT_ID,
-      clientSecret: process.env.DROPBOX_CLIENT_SECRET,
-      authenticate: 'dropbox-oauth2',
-      Strategy: require('passport-dropbox-oauth2').Strategy
-    },
-    twitter: {
-      consumerKey: process.env.TWITTER_CONSUMER_KEY,
-      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-      Strategy: require('passport-twitter').Strategy
-    },
-    github: {
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      Strategy: require('passport-github').Strategy
-    },
-    instagram: {
-      clientID: process.env.INSTAGRAM_CLIENT_ID,
-      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
-    },
-    reddit: {
-      clientID: process.env.REDDIT_CLIENT_ID,
-      clientSecret: process.env.REDDIT_CLIENT_SECRET,
-      Strategy: require('passport-instagram').Strategy
-    },
-    linkedin: {
-      clientID: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-      scope: ['r_basicprofile'],
-      Strategy: require('passport-linkedin-oauth2').Strategy
-    }
-    */
   },
 
   // List supported referrers here, you'll need the complete URL used in the referer.
