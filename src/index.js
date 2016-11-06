bc7b7177067fdd29d71ec52deb61ec6702ab4e95
Jason Sebring <mail@jasonsebring.com>
2016-11-06 09:08:24 -0800

    sqllite

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 const passport = require('passport');
 const passportHelper = require('./utils/passport');
 const session = require('express-session');
+const SQLiteStore = require('connect-sqlite3')(session);
 
 const lex = require('letsencrypt-express').create({
   server: 'production',
@@ -16,6 +17,7 @@ const lex = require('letsencrypt-express').create({
 });
 
 app.use(session({
+  store: new SQLiteStore,
   secret: config.sessionSecret,
   resave: false,
   saveUninitialized: false
